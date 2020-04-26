52df8974a6ac45469e9e9328cbb56c8d0e8d7445
Joao Rocha <jsfrocha@gmail.com>
2020-04-26 20:49:28 +0100

    Environment Variables Test

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: process.env.GRAPHQL_BACKEND_URI,
+    uri: process.env.REACT_APP_GRAPHQL_BACKEND_URI,
     request: (operation) => {
         const token = localStorage.getItem('t4e-token');
         operation.setContext({
